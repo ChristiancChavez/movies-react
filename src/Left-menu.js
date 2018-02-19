56f2a6404b73c036fbe9f583a1dc84666851405c
Christian <christiancchavez10@hotmail.com>
2018-02-19 16:42:10 -0500

    Delete the component SelectLeftMenu and adding the information  tocom

--- src/Left-menu.js
+++ src/Left-menu.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
-import SelectLeftMenu from './select-left-menu.js';
 import ListMovies from './list-movies.js';
 
 class LeftMenu extends Component {
     render () {
         return (
             <div className="left-menu">
-                <SelectLeftMenu />
+                <select className="left-menu__categories" name="select">
+                    <option disabled selected>Select a Movie</option>
+                    <option className="js-favorites" value="favorites">Favorites</option>
+                    <option>Terror</option>
+                    <option>Fantasy</option>
+                    <option>Thriller</option>
+                    <option>Comedy</option>
+                    <option>Romantic</option>
+                    <option>Adventure</option>
+                    <option>Action</option>
+                    <option>War</option>
+                    <option>Western</option>
+                </select>
                 <ListMovies />
             </div>
         );
